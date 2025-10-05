4913e5aa2ecda4bb7908258b0545fd7c73844592
Aurelien <aureplusyt@gmail.com>
2025-10-05 10:55:51 +0200

    pls railway

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -20,5 +20,5 @@ app.use("/api/tags", tagRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT} hoppefully`);
+  console.log(`Server running on http://localhost:${PORT} hoppefully pls`);
 });
