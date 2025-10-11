be6428536c6679e8a43c5ef040df87c8a090e88a
Aurelien <aureplusyt@gmail.com>
2025-10-11 18:18:58 +0200

    pls vercel

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -8,6 +8,7 @@ export function getDb() {
   if (!_db) {
     const pool = new Pool({
       connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
       max: 1,
       idleTimeoutMillis: 10000,
       connectionTimeoutMillis: 10000,
